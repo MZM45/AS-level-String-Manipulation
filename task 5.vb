3363464262d12473243648e6e90a1ff0a00bd62a
Zain Momin <55015838+MZM45@users.noreply.github.com>
2019-10-13 16:19:23 +0500

    Add files via upload

---
+++ task 5.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str As String = ""
+        Dim newstr As String = ""
+        
+        Console.Write(" Enter string: ")
+        str = Console.ReadLine
+
+        str = StrReverse(str)
+        Console.Write("The new string is :" & str)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
