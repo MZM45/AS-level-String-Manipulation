2277b56c31de2359fdd677a055da8765e51d0673
Zain Momin <55015838+MZM45@users.noreply.github.com>
2019-10-13 11:49:55 +0500

    Add files via upload

---
+++ task3.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim input As String
+        Dim counter As Integer
+        Dim newinput As String
+        Dim space As Char
+        Dim input2 As String
+        Dim quitkey As Char
+      
+        input = ""
+        space = " "
+        newinput = ""
+        input2 = ""
+        counter = 0
+        quitkey = ""
+
+        While UCase(quitkey) <> "Y"
+            Console.Write(" Enter string: ")
+            input = Console.ReadLine
+            For counter = 1 To Len(input)
+                input2 = Mid(input, counter, 1)
+                newinput = newinput & input2
+                If input2 = space Then
+                    Console.WriteLine("" & newinput)
+                    newinput = ""
+                End If
+
+            Next
+            Console.WriteLine(newinput)
+            newinput = ""
+            Console.WriteLine(" Press Y to quit or press any other key to repeat")
+            quitkey = Console.ReadLine
+        End While
+    End Sub
+
+End Module
